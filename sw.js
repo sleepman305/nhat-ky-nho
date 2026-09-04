bc71e66c6ce8c8339ef724b3f2cb927f5ab8141b
sleepman305 <sleepman305@users.noreply.github.com>
2026-09-04 19:44:47 +0700

    Bon man rieng: Hom nay, So, Lich, Bia. Trang ngay co nut tien lui

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
   IndexedDB chứ không đi qua đây.
 */
 
-const TEN_DEM = "nhat-ky-nho-v6";
+const TEN_DEM = "nhat-ky-nho-v7";
 
 /** Những thứ phải có sẵn ngay từ lúc cài, không chờ người dùng mở tới. */
 const CAN_TRUOC = [
